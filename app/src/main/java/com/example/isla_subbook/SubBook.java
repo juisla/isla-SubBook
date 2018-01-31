24b8ee1f8d950ce0d9f725b128f5cee045ccb9d5
Julian Isla <isla@cs.ualberta.ca>
2018-01-31 11:45:15 -0700

    initial commit for SubBook

---
+++ app/src/main/java/com/example/isla_subbook/SubBook.java
@@ -0,0 +1,13 @@
+package com.example.isla_subbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_book);
+    }
+}
